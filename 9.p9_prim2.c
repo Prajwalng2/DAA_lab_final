19059a534706ab56f4cd18975cf8b15c649555e8
Prajwal N <118053725+Prajwalng2@users.noreply.github.com>
2023-08-12 22:50:04 +0530

    Update 9.p9_prim2.c

--- 9.p9_prim2.c
+++ 9.p9_prim2.c
@@ -5,75 +5,47 @@
 
 #define V 10 // Maximum number of vertices
 
-// A utility function to find the vertex with
-// minimum key value, from the set of vertices
-// not yet included in MST
-int minKey(int key[], bool mstSet[])
-{
-    // Initialize min value
+int minKey(int key[], bool mstSet[], int vertices) {
     int min = INT_MAX, min_index;
 
-    for (int v = 0; v < V; v++)
+    for (int v = 0; v < vertices; v++)
         if (mstSet[v] == false && key[v] < min)
             min = key[v], min_index = v;
 
     return min_index;
 }
 
-// A utility function to print the
-// constructed MST stored in parent[]
-int printMST(int parent[], int graph[V][V], int vertices)
-{
+int printMST(int parent[], int graph[V][V], int vertices) {
     printf("Edge \tWeight\n");
     for (int i = 1; i < vertices; i++)
         printf("%d - %d \t%d \n", parent[i], i, graph[i][parent[i]]);
 }
 
-// Function to construct and print MST for
-// a graph represented using adjacency
-// matrix representation
-void primMST(int graph[V][V], int vertices)
-{
-    // Array to store constructed MST
+void primMST(int graph[V][V], int vertices, int startVertex) {
     int parent[V];
-    // Key values used to pick minimum weight edge in cut
     int key[V];
-    // To represent set of vertices included in MST
     bool mstSet[V];
 
-    // Initialize all keys as INFINITE
-    for (int i = 0; i < V; i++)
+    for (int i = 0; i < vertices; i++)
         key[i] = INT_MAX, mstSet[i] = false;
 
-    // Always include first 1st vertex in MST.
-    // Make key 0 so that this vertex is picked as first
-    // vertex.
-    key[0] = 0;
-
-    // First node is always root of MST
-    parent[0] = -1;
+    key[startVertex] = 0;
+    parent[startVertex] = -1;
 
     for (int count = 0; count < vertices - 1; count++) {
+        int u = minKey(key, mstSet, vertices);
 
-        // Pick the minimum key vertex from the
-        // set of vertices not yet included in MST
-        int u = minKey(key, mstSet);
-
-        // Add the picked vertex to the MST Set
         mstSet[u] = true;
 
         for (int v = 0; v < vertices; v++)
-            if (graph[u][v] && mstSet[v] == false
-                && graph[u][v] < key[v])
+            if (graph[u][v] && mstSet[v] == false && graph[u][v] < key[v])
                 parent[v] = u, key[v] = graph[u][v];
     }
 
-    // print the constructed MST
     printMST(parent, graph, vertices);
 }
 
-int main()
-{
+int main() {
     int vertices;
 
     printf("Enter the number of vertices (max %d): ", V);
@@ -88,10 +60,13 @@ int main()
         }
     }
 
+    int startVertex;
+    printf("Enter the starting vertex (0 to %d): ", vertices - 1);
+    scanf("%d", &startVertex);
+
     clock_t start_time = clock();
 
-    // Print the solution
-    primMST(graph, vertices);
+    primMST(graph, vertices, startVertex);
 
     clock_t end_time = clock();
     double time_taken = (double)(end_time - start_time) / CLOCKS_PER_SEC;
